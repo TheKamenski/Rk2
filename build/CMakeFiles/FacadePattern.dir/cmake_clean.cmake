7b7f0246523dd42527b65c94db70d83371f70e0f
TheKamenski <k.kamenskiy@gmail.com>
2024-05-25 20:46:42 +0300

    may be done

---
+++ build/CMakeFiles/FacadePattern.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FacadePattern.dir/src/main.cpp.o"
+  "CMakeFiles/FacadePattern.dir/src/main.cpp.o.d"
+  "FacadePattern"
+  "FacadePattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FacadePattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
