38d6e36bc83fd2b64123776c11201048ebe3a2ae
TheKamenski <k.kamenskiy@gmail.com>
2024-05-26 21:41:28 +0300

    level 6

--- test_decorator.cpp
+++ test_decorator.cpp
@@ -1,43 +1,10 @@
 #include <iostream>
 
 
-#include <cassert>
-#include <sstream>
-
-// Функция для тестирования ConcreteComponent
-void testConcreteComponent() {
-  std::stringstream buffer;
-  std::streambuf *prevcoutbuf = std::cout.rdbuf(buffer.rdbuf());
-
-  ConcreteComponent comp;
-  comp.operation();
-
-  std::string output = buffer.str();
-  assert(output == "Concrete Component operation\n");
-
-  std::cout.rdbuf(prevcoutbuf); // Восстановление стандартного вывода
-}
-
-// Функция для тестирования ConcreteDecoratorA
-void testConcreteDecoratorA() {
-  std::stringstream buffer;
-  std::streambuf *prevcoutbuf = std::cout.rdbuf(buffer.rdbuf());
-
-  ConcreteComponent comp;
-  ConcreteDecoratorA decA(&comp);
-  decA.operation();
-
-  std::string output = buffer.str();
-  assert(output == "Concrete Component operation\nDecorator A\n");
-
-  std::cout.rdbuf(prevcoutbuf); // Восстановление ст
-
-
 // Функция для запуска всех тестов
 void runTests()
 {
-  testConcreteComponent()
-  testConcreteDecoratorA()
+
   }
 
 int main()
