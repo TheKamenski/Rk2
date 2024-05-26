ea26194bd258af02f8847dcfbb5fb923ae7b8440
TheKamenski <k.kamenskiy@gmail.com>
2024-05-26 11:26:51 +0300

    may be done

--- tests/test_facade.cpp
+++ tests/test_facade.cpp
@@ -1,53 +1,47 @@
-#include <iostream>
-#include <cassert>
+
 #include <sstream>
+#include <string>
 
-// Предыдущий код классов остается без изменений
+ //  Подключите ваш заголовочный файл с классами
 
-// Тестовая функция для проверки ConcreteComponent
-void test_concrete_component() {
-    ConcreteComponent cc;
-    std::stringstream ss;
-    std::streambuf* oldCoutBuffer = std::cout.rdbuf(ss.rdbuf());
+// Тест для ConcreteComponent
+TEST(DecoratorTest, ConcreteComponentOperation) {
+  ConcreteComponent component;
+  std::stringstream ss;
+  std::streambuf * oldCout = std::cout.rdbuf();
+  std::cout.rdbuf(ss.rdbuf());
 
-    cc.operation();
+  component.operation();
 
-    std::cout.rdbuf(oldCoutBuffer);
-    assert(ss.str() == "Concrete Component operation\n");
+  std::cout.rdbuf(oldCout);
+  EXPECT_EQ(ss.str(), "Concrete Component operation\n");
 }
 
-// Тестовая функция для проверки ConcreteDecoratorA
-void test_concrete_decorator_a() {
-    ConcreteComponent cc;
-    ConcreteDecoratorA da(&cc);
-    std::stringstream ss;
-    std::streambuf* oldCoutBuffer = std::cout.rdbuf(ss.rdbuf());
+// Тест для ConcreteDecoratorA
+TEST(DecoratorTest, ConcreteDecoratorAOperation) {
+  ConcreteComponent component;
+  ConcreteDecoratorA decoratorA(&component);
+  std::stringstream ss;
+  std::streambuf * oldCout = std::cout.rdbuf();
+  std::cout.rdbuf(ss.rdbuf());
 
-    da.operation();
+  decoratorA.operation();
 
-    std::cout.rdbuf(oldCoutBuffer);
-    assert(ss.str() == "Concrete Component operation\nDecorator A\n");
+  std::cout.rdbuf(oldCout);
+  EXPECT_EQ(ss.str(), "Concrete Component operation\nDecorator A\n");
 }
 
-// Тестовая функция для проверки ConcreteDecoratorB
-void test_concrete_decorator_b() {
-    ConcreteComponent cc;
-    ConcreteDecoratorB db(&cc);
-    std::stringstream ss;
-    std::streambuf* oldCoutBuffer = std::cout.rdbuf(ss.rdbuf());
+// Тест для ConcreteDecoratorB
+TEST(DecoratorTest, ConcreteDecoratorBOperation) {
+  ConcreteComponent component;
+  ConcreteDecoratorB decoratorB(&component);
+  std::stringstream ss;
+  std::streambuf * oldCout = std::cout.rdbuf();
+  std::cout.rdbuf(ss.rdbuf());
 
-    db.operation();
+  decoratorB.operation();
 
-    std::cout.rdbuf(oldCoutBuffer);
-    assert(ss.str() == "Concrete Component operation\nDecorator B\n");
-}
-
-int main() {
-    test_concrete_component();
-    test_concrete_decorator_a();
-    test_concrete_decorator_b();
-
-    std::cout << "All tests passed!" << std::endl;
-    return0;
+  std::cout.rdbuf(oldCout);
+  EXPECT_EQ(ss.str(), "Concrete Component operation\nDecorator B\n");
 }
 
