f0cdef3346b6ee263afa49559f24af2d5c6917e9
TheKamenski <k.kamenskiy@gmail.com>
2024-05-26 11:30:24 +0300

    may be done

--- CMakeFiles/CMakeDirectoryInformation.cmake
+++ CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/home/konstantin/Rk2")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/konstantin/Rk2/build")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/konstantin/Rk2")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/konstantin/Rk2/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/konstantin/Rk2/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
