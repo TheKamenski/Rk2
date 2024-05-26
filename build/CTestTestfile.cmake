1bf92631a81edd8bce2a2c9bd1d91e3b1e2eb5e5
TheKamenski <k.kamenskiy@gmail.com>
2024-05-26 21:41:13 +0300

    level 6

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/konstantin/decorator-pattern-cpp
+# Build directory: /home/konstantin/decorator-pattern-cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(DecoratorPatternTest "/home/konstantin/decorator-pattern-cpp/build/DecoratorPatternTest")
+set_tests_properties(DecoratorPatternTest PROPERTIES  _BACKTRACE_TRIPLES "/home/konstantin/decorator-pattern-cpp/CMakeLists.txt;22;add_test;/home/konstantin/decorator-pattern-cpp/CMakeLists.txt;0;")
