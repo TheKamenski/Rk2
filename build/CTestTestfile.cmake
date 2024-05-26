c755068a4a9e1277d3c0c0ccca4984d2569b1b02
TheKamenski <k.kamenskiy@gmail.com>
2024-05-26 11:36:54 +0300

    may be done

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /home/konstantin/Rk2
-# Build directory: /home/konstantin/Rk2/build
+# Source directory: /home/vadim/rk2
+# Build directory: /home/vadim/rk2/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(MyTest "test")
-set_tests_properties(MyTest PROPERTIES  _BACKTRACE_TRIPLES "/home/konstantin/Rk2/CMakeLists.txt;18;add_test;/home/konstantin/Rk2/CMakeLists.txt;0;")
+add_test(SingletonTest "/home/vadim/rk2/build/SingletonTest")
+set_tests_properties(SingletonTest PROPERTIES  _BACKTRACE_TRIPLES "/home/vadim/rk2/CMakeLists.txt;20;add_test;/home/vadim/rk2/CMakeLists.txt;0;")
